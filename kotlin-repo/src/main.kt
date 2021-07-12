85ff872adb510663ad9ddb82110431989f9dde56
Ezequiel Gnich <ezequielgnich@live.com>
2021-07-12 19:04:19 -0300

    first commit

---
+++ kotlin-repo/src/main.kt
@@ -0,0 +1,18 @@
+/**
+ * Type     bit
+ * Double   64
+ * Float    32
+ * Long     64
+ * Int      32
+ * Short    16
+ * Byte     8
+ * Boolean  ?
+ * String   ?
+ * Char     ?
+ * */
+
+fun main() {
+    var name: String = "Ezequiel Gnich";
+
+    println("Hello world $name")
+}
