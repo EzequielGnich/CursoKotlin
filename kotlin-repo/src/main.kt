3e9f3118af4cedd3a12b0442b0783f3b665984b8
Ezequiel Gnich <ezequielgnich@live.com>
2021-07-12 19:20:14 -0300

    Variáveis mutaveis e imutáveis

--- kotlin-repo/src/main.kt
+++ kotlin-repo/src/main.kt
@@ -16,10 +16,17 @@ fun main() {
 
     println("Hello world $name")
 
-    println("Double: Max.: ${Double.MAX_VALUE} / Min.: ${Double.MIN_VALUE}")
-    println("Float: Max.: ${Float.MAX_VALUE} / Min.: ${Float.MIN_VALUE}")
-    println("Long: Max.: ${Long.MAX_VALUE} / Min.: ${Long.MIN_VALUE}")
-    println("Int: Max.: ${Int.MAX_VALUE} / Min.: ${Int.MIN_VALUE}")
-    println("Short: Max.: ${Short.MAX_VALUE} / Min.: ${Short.MIN_VALUE}")
-    println("Byte: Max.: ${Byte.MAX_VALUE} / Min.: ${Byte.MIN_VALUE}")
+    // println("Double: Max.: ${Double.MAX_VALUE} / Min.: ${Double.MIN_VALUE}")
+    // println("Float: Max.: ${Float.MAX_VALUE} / Min.: ${Float.MIN_VALUE}")
+    // println("Long: Max.: ${Long.MAX_VALUE} / Min.: ${Long.MIN_VALUE}")
+    // println("Int: Max.: ${Int.MAX_VALUE} / Min.: ${Int.MIN_VALUE}")
+    // println("Short: Max.: ${Short.MAX_VALUE} / Min.: ${Short.MIN_VALUE}")
+    // println("Byte: Max.: ${Byte.MAX_VALUE} / Min.: ${Byte.MIN_VALUE}")
+
+    // Variáveis mutáveis
+    var nome: String = "João da Silva"
+    nome = "João da silva 2"
+    
+    // Variáveis imutáveis
+    val age: Int = 10
 }
