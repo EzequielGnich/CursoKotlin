5f2077e29fa64c60c0b9652e8d6141c4df0908ff
Ezequiel Gnich <ezequielgnich@live.com>
2021-07-13 18:45:31 -0300

    While and Do while

---
+++ kotlin-repo/src/doWhile.kt
@@ -0,0 +1,6 @@
+fun main() {
+    var index = 10
+    do {
+        println(index)
+    } while (index > 100)
+}
---
+++ kotlin-repo/src/while.kt
@@ -0,0 +1,16 @@
+fun main() {
+    var index: Int = 0
+    while (index < 10) {
+        println("Ezequiel")
+        index++
+    }
+
+    while (index < 10) {
+        println("Ezequiel")
+        index++
+
+        if (index == 5) {
+            break
+        }
+    }
+}
