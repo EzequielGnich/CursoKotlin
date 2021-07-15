9f41a5eb1869f80bb5ca29f238d62a2c6067d0e6
Ezequiel Gnich <ezequielgnich@live.com>
2021-07-14 23:13:44 -0300

    Implementing delete contact

--- Schedule/src/repository/ContactRepository.kt
+++ Schedule/src/repository/ContactRepository.kt
@@ -11,7 +11,16 @@ class ContactRepository {
         }
 
         fun deleteContact(contact: ContactEntity) {
+            // contactList.remove(contact)
 
+            var index = 0
+            for (item in contactList.withIndex()) {
+                if (item.value.name == contact.name && item.value.phone == contact.phone) {
+                    index = item.index
+                    break
+                }
+            }
+            contactList.removeAt(index)
         }
 
         fun getContactList(): List<ContactEntity> {
